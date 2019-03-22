ff5b82b7b6d9424b97926e24ba4861f134139724
shayanmalinda <shayanmalinda@gmail.com>
2019-03-22 11:01:33 +0530

    Backend Completed

---
+++ app/src/main/java/com/im/easycontacts/ViewContactActivity.java
@@ -0,0 +1,13 @@
+package com.im.easycontacts;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ViewContactActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_contact);
+    }
+}
